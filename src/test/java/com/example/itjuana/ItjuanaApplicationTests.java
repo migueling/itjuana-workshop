93de71263f93957949d0cea8546c7bb5fa64026c
Miguel Angel Sanchez Solis <ing.miguels@gmail.com>
2021-10-01 17:05:57 -0500

    First commit

---
+++ src/test/java/com/example/itjuana/ItjuanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itjuana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItjuanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
